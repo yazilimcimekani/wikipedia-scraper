27d4e0b2183dd4fcc20bd6c9c408beb38f719cbf
Mert Şişmanoğlu <mert190737fb@gmail.com>
2021-04-18 18:24:12 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,19 +4,15 @@ from bs4 import BeautifulSoup
 now = datetime.datetime.now()
 database = open("config.json")
 data = json.load(database)
-USER_AGENT = data["user"]["USER_AGENT"]
+USER_AGENT = data['user']['USER_AGENT']
 systemLang = data["user"]["language"]
-sep = data["system"][systemLang]["sep"]
-if systemLang in data["system"]:
+
+if systemLang in data['system']:
     bugun = now.day
     ay = data["system"][systemLang]["months"]["_" + str(now.month)]
-    startupText = data["system"][systemLang]["sentences"]["startupText"].format(
-        day=bugun, month=ay, year=now.year
-    )
+    startupText = data["system"][systemLang]["sentences"]["startupText"].format(day=bugun, month=ay, year=now.year)
     url = f"https://{systemLang}.wikipedia.org/wiki/{bugun}_{ay}"
-    inputText = data["system"][systemLang]["sentences"]["inputText"].format(
-        confirm=data["system"][systemLang]["sentences"]["confirm"].lower()
-    )
+    inputText = data["system"][systemLang]["sentences"]["inputText"].format(confirm=data["system"][systemLang]["sentences"]["confirm"].lower())
     processText = data["system"][systemLang]["sentences"]["processText"].format(url=url)
     confirmText = data["system"][systemLang]["sentences"]["confirm"].lower()
     print(f"<<< {startupText} >>>")
@@ -29,8 +25,7 @@ if systemLang in data["system"]:
         soup = BeautifulSoup(content, "html.parser")
         all_Lists = soup.find_all("li", {"class": "", "id": ""})
         for job in all_Lists:
-            theindex = str(job.text).find(sep)
-            if(theindex != -1):
-                print(job.text)
+            print(job.text)
+
 else:
     print(f"{systemLang} is not supported :/")
