f58f1647b1b2cf9df356d1c8c40c18f12f78b476
Mert Şişmanoğlu <mert190737fb@gmail.com>
2021-04-18 17:11:24 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# **Wikipedia "Today in History" Scraper**
+
+The app gets all "Today in History" datas from wikipedia.
+
+<p align="center">
+<img src="https://upload.wikimedia.org/wikipedia/commons/6/63/Wikipedia-logo.png" alt="Wikipedia Logo"></img>
+</p>
+
+## Download
+
+```bash
+git clone https://github.com/mertssmnoglu/wikipedia-scraper
+```
+## Usage
+
+First of all open the **config.json** file and enter your `User Agent` and `Language`. Now all you need to do is confirm that scraping in the app.
+
+## Lisence
+
+MIT Lisenced. Read `LISENCE` before use.
---
+++ main.py
@@ -0,0 +1,31 @@
+import requests, json, datetime
+from bs4 import BeautifulSoup
+
+now = datetime.datetime.now()
+database = open("config.json")
+data = json.load(database)
+USER_AGENT = data['user']['USER_AGENT']
+systemLang = data["user"]["language"]
+
+if systemLang in data['system']:
+    bugun = now.day
+    ay = data["system"][systemLang]["months"]["_" + str(now.month)]
+    startupText = data["system"][systemLang]["sentences"]["startupText"].format(day=bugun, month=ay, year=now.year)
+    url = f"https://{systemLang}.wikipedia.org/wiki/{bugun}_{ay}"
+    inputText = data["system"][systemLang]["sentences"]["inputText"].format(confirm=data["system"][systemLang]["sentences"]["confirm"].lower())
+    processText = data["system"][systemLang]["sentences"]["processText"].format(url=url)
+    confirmText = data["system"][systemLang]["sentences"]["confirm"].lower()
+    print(f"<<< {startupText} >>>")
+    answer = input(inputText)
+    if answer.lower() == confirmText:
+        database.close()
+        print(processText)
+        params = {"User Agent": USER_AGENT}
+        content = requests.get(url, params).content
+        soup = BeautifulSoup(content, "html.parser")
+        all_Lists = soup.find_all("li", {"class": "", "id": ""})
+        for job in all_Lists:
+            print(job.text)
+
+else:
+    print(f"{systemLang} is not supported :/")
